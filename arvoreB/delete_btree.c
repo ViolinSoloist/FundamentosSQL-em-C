f17bd03697ac5049aee14ede6dd7587f0220f76c
Kire_minsoo <chungerik7@gmail.com>
2026-06-16 11:25:31 -0300

    refactor: funcionalidade delecao

--- arvoreB/delete_btree.c
+++ arvoreB/delete_btree.c
@@ -1,3 +1,5 @@
+/// @author Erik
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -131,81 +133,127 @@ static bool registroPassaFiltro(Registro* reg, OQueBuscar* query) {
     return true;
 }
 
-static void executarRemocaoPorCriterio(FILE* fileDados, FILE* fileIndice, CabecalhoArvore* cabIndice, OQueBuscar* query) {
+// ---------------- FUNÇÕES RELACIONADAS Á BUSCA PARA PODER DELETAR -----------------------
+
+// struct com parâmetros usados nessas funções
+typedef struct {
+    FILE* fileDados;
+    FILE* fileIndice;
+    CabecalhoArvore* cabIndice;
+    OQueBuscar* query;
+} ParamBuscaDelecao;
+
+/// @brief se o que está sendo buscado é a chave (codEstacao), usa-se busca em árvore-B
+static void buscaUsaArvoreRemove(ParamBuscaDelecao* args) {
     
-    // busca chave (codEstacao)? => Usa Arvore
-    if (query->checar_codEstacao) {
-    
-        long byteOffset = ArvoreBuscar(fileIndice, cabIndice, query->valores.codEstacao);
+    long byteOffset = ArvoreBuscar(args->fileIndice, args->cabIndice, args->query->valores.codEstacao);
         
-        if (byteOffset != -1) { 
-            fseek(fileDados, byteOffset, SEEK_SET);
-            char removido;
-            fread(&removido, sizeof(char), 1, fileDados);
-            
-            Registro reg;
-            binToStruct(&reg, fileDados); 
-            
-            if (removido == '0' && registroPassaFiltro(&reg, query)) {
-                
-                // convete pra RRN pra empilhar no topo
-                int rrn_dados = (byteOffset - 17) / 80;
-
-                int topo_dados;
-                fseek(fileDados, 1, SEEK_SET);
-                fread(&topo_dados, sizeof(int), 1, fileDados);
-                
-                fseek(fileDados, byteOffset, SEEK_SET);
-                char marcaRemovido = '1';
-                fwrite(&marcaRemovido, sizeof(char), 1, fileDados);
-                fwrite(&topo_dados, sizeof(int), 1, fileDados);
-                
-                fseek(fileDados, 1, SEEK_SET);
-                fwrite(&rrn_dados, sizeof(int), 1, fileDados);
-                
-                // remoção na árbore (boa sorte)
-                ArvoreRemover(fileIndice, cabIndice, reg.codEstacao);
-            }
-            
-            if (reg.nomeEstacao) { free(reg.nomeEstacao); reg.nomeEstacao = NULL; }
-            if (reg.nomeLinha) { free(reg.nomeLinha); reg.nomeLinha = NULL; }
-        }
-    } 
-
-    // qualquer outra coisa sendo buscada? usa iteração pelo arquivo normal (chega de árvore plmds)
-    else {
-        fseek(fileDados, 17, SEEK_SET);
-        int rrn_atual = 0;
+    if (byteOffset != -1) { 
+        fseek(args->fileDados, byteOffset, SEEK_SET);
         char removido;
-        Registro reg;
+        fread(&removido, sizeof(char), 1, args->fileDados);
         
-        while (fread(&removido, sizeof(char), 1, fileDados) == 1) {
-            binToStruct(&reg, fileDados); 
+        Registro reg;
+        binToStruct(&reg, args->fileDados); 
+        
+        if (removido == '0' && registroPassaFiltro(&reg, args->query)) {
             
-            if (removido == '0' && registroPassaFiltro(&reg, query)) {
-                int topo_dados;
-                fseek(fileDados, 1, SEEK_SET);
-                fread(&topo_dados, sizeof(int), 1, fileDados);
-                
-                long byteOffset = 17 + (rrn_atual * 80);
-                fseek(fileDados, byteOffset, SEEK_SET);
-                char marcaRemovido = '1';
-                fwrite(&marcaRemovido, sizeof(char), 1, fileDados);
-                fwrite(&topo_dados, sizeof(int), 1, fileDados);
-                
-                fseek(fileDados, 1, SEEK_SET);
-                fwrite(&rrn_atual, sizeof(int), 1, fileDados);
-                
-                // usando arvore ou não pra achar, se você removeu tem que tirar da arvore também
-                ArvoreRemover(fileIndice, cabIndice, reg.codEstacao);
-            }
+            // convete pra RRN pra empilhar no topo
+            int rrn_dados = (byteOffset - 17) / 80;
+
+            int topo_dados;
+            fseek(args->fileDados, 1, SEEK_SET);
+            fread(&topo_dados, sizeof(int), 1, args->fileDados);
             
-            if (reg.nomeEstacao) { free(reg.nomeEstacao); reg.nomeEstacao = NULL;} // Remoção Física na Árvore-B
-            if (reg.nomeLinha) { free(reg.nomeLinha); reg.nomeLinha = NULL; }
+            fseek(args->fileDados, byteOffset, SEEK_SET);
+            char marcaRemovido = '1';
+            fwrite(&marcaRemovido, sizeof(char), 1, args->fileDados);
+            fwrite(&topo_dados, sizeof(int), 1, args->fileDados);
             
-            rrn_atual++;
-            fseek(fileDados, 17 + (rrn_atual * 80), SEEK_SET); 
+            fseek(args->fileDados, 1, SEEK_SET);
+            fwrite(&rrn_dados, sizeof(int), 1, args->fileDados);
+            
+            // remoção na árbore (boa sorte)
+            ArvoreRemover(args->fileIndice, args->cabIndice, reg.codEstacao);
         }
+        
+        if (reg.nomeEstacao) { free(reg.nomeEstacao); reg.nomeEstacao = NULL; }
+        if (reg.nomeLinha) { free(reg.nomeLinha); reg.nomeLinha = NULL; }
+    }
+}
+
+// qualquer outro registro que não seja codEstacao é buscado de maneira "normal", sequencial, pelo arquivo
+static void buscaNormalRemove(ParamBuscaDelecao* args) {
+
+    fseek(args->fileDados, 17, SEEK_SET);
+    int rrn_atual = 0;
+    char removido;
+    Registro reg;
+    
+    while (fread(&removido, sizeof(char), 1, args->fileDados) == 1) {
+        binToStruct(&reg, args->fileDados); 
+        
+        if (removido == '0' && registroPassaFiltro(&reg, args->query)) {
+            int topo_dados;
+            fseek(args->fileDados, 1, SEEK_SET);
+            fread(&topo_dados, sizeof(int), 1, args->fileDados);
+            
+            long byteOffset = 17 + (rrn_atual * 80);
+            fseek(args->fileDados, byteOffset, SEEK_SET);
+            char marcaRemovido = '1';
+            fwrite(&marcaRemovido, sizeof(char), 1, args->fileDados);
+            fwrite(&topo_dados, sizeof(int), 1, args->fileDados);
+            
+            fseek(args->fileDados, 1, SEEK_SET);
+            fwrite(&rrn_atual, sizeof(int), 1, args->fileDados);
+            
+            // usando arvore ou não pra achar, se você removeu tem que tirar da arvore também
+            ArvoreRemover(args->fileIndice, args->cabIndice, reg.codEstacao);
+        }
+        
+        if (reg.nomeEstacao) { free(reg.nomeEstacao); reg.nomeEstacao = NULL;} // Remoção Física na Árvore-B
+        if (reg.nomeLinha) { free(reg.nomeLinha); reg.nomeLinha = NULL; }
+        
+        rrn_atual++;
+        fseek(args->fileDados, 17 + (rrn_atual * 80), SEEK_SET); 
+    }
+}
+
+/// @brief função manual reponsável por manualmente preencher a query de busca, para contornar bug do scanquotestring
+static void getQueryManualmente(OQueBuscar* query) {
+    char nomeCampo[67];
+    scanf("%s", nomeCampo);
+
+    if (strcmp(nomeCampo, "codEstacao") == 0) {
+        query->checar_codEstacao = true;
+        scanf("%d", &query->valores.codEstacao);
+    } else if (strcmp(nomeCampo, "codLinha") == 0) {
+        query->checar_codLinha = true;
+        scanf("%d", &query->valores.codLinha);
+    } else if (strcmp(nomeCampo, "codProxEstacao") == 0) {
+        query->checar_codProxEstacao = true;
+        scanf("%d", &query->valores.codProxEstacao);
+    } else if (strcmp(nomeCampo, "distProxEstacao") == 0) {
+        query->checar_distProxEstacao = true;
+        scanf("%d", &query->valores.distProxEstacao);
+    } else if (strcmp(nomeCampo, "codLinhaIntegra") == 0) {
+        query->checar_codLinhaIntegra = true;
+        scanf("%d", &query->valores.codLinhaIntegra);
+    } else if (strcmp(nomeCampo, "codEstIntegra") == 0) {
+        query->checar_codEstIntegra = true;
+        scanf("%d", &query->valores.codEstIntegra);
+    } else if (strcmp(nomeCampo, "nomeEstacao") == 0) {
+        query->checar_nomeEstacao = true;
+        char buffer[200];
+        ScanQuoteString(buffer);
+        query->valores.nomeEstacao = malloc(strlen(buffer) + 1);
+        strcpy(query->valores.nomeEstacao, buffer);
+    } else if (strcmp(nomeCampo, "nomeLinha") == 0) {
+        query->checar_nomeLinha = true;
+        char buffer[200];
+        ScanQuoteString(buffer);
+        query->valores.nomeLinha = malloc(strlen(buffer) + 1);
+        strcpy(query->valores.nomeLinha, buffer);
     }
 }
 
@@ -214,18 +262,17 @@ static void executarRemocaoPorCriterio(FILE* fileDados, FILE* fileIndice, Cabeca
 /// @param nomeArquivoIndice 
 /// @param n 
 void delete_btree(const char* nomeArquivoDados, const char* nomeArquivoIndice, int n) {
+    
     FILE* fileDados = rotinaAbrirArquivo(nomeArquivoDados, ESCRITA);
     if (fileDados == NULL) return;
 
     FILE* fileIndice = rotinaAbrirArquivo(nomeArquivoIndice, ESCRITA);
-    if (fileIndice == NULL) {
-        finalizarArquivoEscrita(fileDados, false);
-        return;
-    }
+    if (fileIndice == NULL) { finalizarArquivoEscrita(fileDados, false); return;}
 
     CabecalhoArvore cabIndice;
     lerCabecalhoArvore(fileIndice, &cabIndice);
 
+    ParamBuscaDelecao args; // parametros das funções que organizam o tipo busca (para remoção, não confundir com a funcionalidade de busca em si)
     for (int i=0; i<n; i++) {
         OQueBuscar query;
         memset(&query, 0, sizeof(OQueBuscar));
@@ -233,51 +280,20 @@ void delete_btree(const char* nomeArquivoDados, const char* nomeArquivoIndice, i
         int m;
         scanf("%d", &m); 
         
-        /// @attention GAMBIARRA AFRENTE!!!!!!!!! TEMPORÁRIO PARA CONTORNAR BUG DO ScanQuoteString
-        for (int j=0; j<m; j++) {
-            char nomeCampo[50];
-            scanf("%s", nomeCampo);
-
-            if (strcmp(nomeCampo, "codEstacao") == 0) {
-                query.checar_codEstacao = true;
-                scanf("%d", &query.valores.codEstacao);
-            } else if (strcmp(nomeCampo, "codLinha") == 0) {
-                query.checar_codLinha = true;
-                scanf("%d", &query.valores.codLinha);
-            } else if (strcmp(nomeCampo, "codProxEstacao") == 0) {
-                query.checar_codProxEstacao = true;
-                scanf("%d", &query.valores.codProxEstacao);
-            } else if (strcmp(nomeCampo, "distProxEstacao") == 0) {
-                query.checar_distProxEstacao = true;
-                scanf("%d", &query.valores.distProxEstacao);
-            } else if (strcmp(nomeCampo, "codLinhaIntegra") == 0) {
-                query.checar_codLinhaIntegra = true;
-                scanf("%d", &query.valores.codLinhaIntegra);
-            } else if (strcmp(nomeCampo, "codEstIntegra") == 0) {
-                query.checar_codEstIntegra = true;
-                scanf("%d", &query.valores.codEstIntegra);
-            } else if (strcmp(nomeCampo, "nomeEstacao") == 0) {
-                query.checar_nomeEstacao = true;
-                char buffer[200];
-                ScanQuoteString(buffer);
-                query.valores.nomeEstacao = malloc(strlen(buffer) + 1);
-                strcpy(query.valores.nomeEstacao, buffer);
-            } else if (strcmp(nomeCampo, "nomeLinha") == 0) {
-                query.checar_nomeLinha = true;
-                char buffer[200];
-                ScanQuoteString(buffer);
-                query.valores.nomeLinha = malloc(strlen(buffer) + 1);
-                strcpy(query.valores.nomeLinha, buffer);
-            }
-        }
+        for (int j=0; j<m; j++) getQueryManualmente(&query); // função ScanQuoteString bugada? => checkagem manual
         
-        executarRemocaoPorCriterio(fileDados, fileIndice, &cabIndice, &query);
+        // ---------- EXECUTA BUSCA SEQUENCIAL/ARVORE DO REGISTRO A SER REMOVIDO E REMOVE ------------------
+        args.fileDados = fileDados;     args.fileIndice = fileIndice;   args.cabIndice = &cabIndice;      args.query = &query;
+        
+        if (query.checar_codEstacao) buscaUsaArvoreRemove(&args); // busca chave (codEstacao)? => Usa Arvore, essa função também chama a remoção em arvore
 
+        else buscaNormalRemove(&args); // qualquer outra coisa sendo buscada? usa iteração pelo arquivo normal, também chama remoção em arvore (chega de árvore plmds)
+
+        // liberação de memória
         if (query.checar_nomeEstacao && query.valores.nomeEstacao) free(query.valores.nomeEstacao);
         if (query.checar_nomeLinha && query.valores.nomeLinha) free(query.valores.nomeLinha);
     }
 
-    // finalização
     recalcularContadores(fileDados);
 
     finalizarArquivoEscrita(fileDados, false);
--- arvoreB/delete_btree.h
+++ arvoreB/delete_btree.h
@@ -4,12 +4,9 @@
 #include "estruturas.h"
 
 /**
- * delete_btree - Remove registros da árvore B e arquivo de dados conforme critérios
- * @nomeArquivoDados: caminho do arquivo de dados
- * @nomeArquivoIndice: caminho do arquivo de índice (árvore B)
- * @n: número de consultas/remoções a executar
+ * delete_btree - remove registros da árvore B e arquivo de dados conforme critérios
  * 
- * Executa n operações de remoção lógica no arquivo de dados e remove as chaves
+ * executa n operações de remoção lógica no arquivo de dados e remove as chaves
  * correspondentes do índice em árvore B.
  */
 void delete_btree(const char* nomeArquivoDados, const char* nomeArquivoIndice, int n);
