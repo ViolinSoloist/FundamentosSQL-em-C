f3acd1b13ae044f7ab188e7a1f6e51b97500ea5e
ViolinSoloist <chungerik7@gmail.com>
2026-07-07 20:32:30 -0300

    Refactor: funcionalidade 12

--- juncao/select_where_juncao.c
+++ juncao/select_where_juncao.c
@@ -6,16 +6,15 @@ static void processarLoopInterno(FILE* bin2, Registro* temp1, const char* campo1
     fseek(bin2, OFFSET_INI_DADOS, SEEK_SET); // volta pro começo do arquivo 2
 
     while(fread(&removido2, sizeof(char), 1, bin2) == 1){
-        if (removido2 == '1'){
-            fseek(bin2, TAM_REGISTRO-1, SEEK_CUR);
-        } else {
+        if (removido2 == '1') fseek(bin2, TAM_REGISTRO-1, SEEK_CUR);
+        else {
             Registro temp2;
             binToStruct(&temp2, bin2);
             
             // achou? imprime o registro da junção
             if (comparaCampos(temp1, campo1, &temp2, campo2)) printf("%d %s %s %d %s\n", temp1->codEstacao, temp1->nomeEstacao, temp1->nomeLinha, temp1->codProxEstacao, temp2.nomeEstacao);
             
-
+            // libera memória a no final do loop
             if (temp2.nomeEstacao) free(temp2.nomeEstacao);
             if (temp2.nomeLinha) free(temp2.nomeLinha);
         }
@@ -37,9 +36,8 @@ void select_where_juncao(const char* nomeArquivoBin1, const char* campo1, const 
     fseek(bin1, OFFSET_INI_DADOS, SEEK_SET);
     
     while(fread(&removido1, sizeof(char), 1, bin1) == 1){
-        if (removido1 == '1'){
-            fseek(bin1, TAM_REGISTRO-1, SEEK_CUR);
-        } else {
+        if (removido1 == '1') fseek(bin1, TAM_REGISTRO-1, SEEK_CUR);
+        else {
             Registro temp1;
             binToStruct(&temp1, bin1);
             
